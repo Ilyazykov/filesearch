69ce34514f2855bba9517c9d9f192eb507f16a0d
ilyazykov <ilya.zykov@icloud.com>
2017-04-26 01:05:47 +0300

    rename PrefixTreeNode files

--- PrefixTree.c
+++ PrefixTree.c
@@ -1,30 +1,86 @@
-#include <stdio.h>
+#include "PrefixTreeNode.h"
 
-#include "PrefixTree.h"
-
-void addToTree(PrefixTree* prefixTree, const char const * word) {
-    //TODO
-    printf("%s", word);
-}
-
-int readTreeFromFile(PrefixTree* prefixTree, const char const * fileName) {
+int readTreeFromFile(PrefixTreeNode* prefixTree, const char* fileName) {
     FILE *file = fopen(fileName, "r");
     char resultString[MAX_STRING_LENGHT];
 
     if (file == NULL) {
-        return -1;
+        return NULL;
     }
 
     while (fgets(resultString, sizeof(resultString), file)) {
-        addToTree(prefixTree, resultString);
+        if (resultString[strlen(resultString) - 1] == '\n') {
+            resultString[strlen(resultString) - 1] = '\0';
+        }
+
+        prefixTree = addToTree(prefixTree, resultString);
     }
 
     fclose(file);
 
-    return 0;
+    return prefixTree;
 }
 
-int isWordInTree(PrefixTree prefixTree, const char const * word) {
-    //TODO
+PrefixTreeNode* addToTree(PrefixTreeNode* prefixTree, const char* word) {
+    int i = 0;
+    PrefixTreeNode* iteratorCurr;
+    PrefixTreeNode* iteratorNext;
+
+    if (prefixTree == NULL) {
+        prefixTree = (PrefixTreeNode *)malloc(sizeof(PrefixTreeNode));
+        prefixTree->isWord = FALSE;
+        prefixTree->prefixTreeNodes = NULL;
+    }
+    iteratorCurr = prefixTree;
+
+    while (word[i] != '\0') {
+        iteratorNext = (PrefixTreeNode*)(searchInTree(iteratorCurr->prefixTreeNodes, word[i]));
+
+        if (iteratorNext == NULL) {
+            iteratorNext = (PrefixTreeNode *)malloc(sizeof(PrefixTreeNode));
+            iteratorNext->isWord = FALSE;
+            iteratorNext->prefixTreeNodes = NULL;
+
+            iteratorCurr->prefixTreeNodes = insert(iteratorCurr->prefixTreeNodes, word[i], iteratorNext);
+        }
+        iteratorCurr = iteratorNext;
+
+        ++i;
+    }
+    iteratorCurr->isWord = TRUE;
+
+    return prefixTree;
 }
 
+int isWordInTree(const PrefixTreeNode* prefixTree, const char* word) {
+    int i = 0;
+    PrefixTreeNode* iterator = prefixTree;
+
+    if (iterator == NULL) {
+        return FALSE;
+    }
+
+    while (word[i] != '\0') {
+        iterator = (PrefixTreeNode*)(searchInTree(iterator->prefixTreeNodes, word[i]));
+        if (iterator == NULL) {
+            return FALSE;
+        }
+
+        ++i;
+    }
+
+    if (iterator->isWord) {
+        return TRUE;
+    } else {
+        return FALSE;
+    }
+}
+
+PrefixTreeNode* deletePrefixTree(PrefixTreeNode* prefixTreeRoot) {
+    PrefixTreeNode*(*ptr)(PrefixTreeNode*) = deletePrefixTree;
+
+    deleteTree(prefixTreeRoot->prefixTreeNodes, deletePrefixTree);
+    free(prefixTreeRoot);
+
+    return NULL;
+}
--- PrefixTree.h
+++ PrefixTree.h
@@ -1,17 +1,24 @@
 #ifndef UNTITLED_PREFIXTREE_H
 #define UNTITLED_PREFIXTREE_H
 
+#include <stdio.h>
+#include <string.h>
 #include "RedBlackTree.h"
 
 #define MAX_STRING_LENGHT 20
+#define TRUE 1
+#define FALSE 0
 
-struct PrefixTree {
-    //TODO
+struct PrefixTreeNode {
+    Node* prefixTreeNodes;
+    int isWord;
 };
-typedef struct PrefixTree PrefixTree;
+typedef struct PrefixTreeNode PrefixTreeNode;
 
-void addToTree(PrefixTree* prefixTree, const char const * word);
-int readTreeFromFile(PrefixTree* prefixTree, const char const * fileName);
-int isWordInTree(PrefixTree prefixTree, const char const * word);
+int readTreeFromFile(PrefixTreeNode* prefixTree, const char* fileName);
+PrefixTreeNode* addToTree(PrefixTreeNode* prefixTree, const char* word);
+int isWordInTree(const PrefixTreeNode* prefixTree, const char* word);
+
+PrefixTreeNode* deletePrefixTree(PrefixTreeNode* prefixTreeRoot);
 
 #endif //UNTITLED_PREFIXTREE_H
