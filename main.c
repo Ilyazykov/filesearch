e410411b973e8d4ab10b6b43794461c6baf7fc1c
ilyazykov <ilya.zykov@icloud.com>
2017-04-26 01:33:01 +0300

    makefile added

--- main.c
+++ main.c
@@ -8,16 +8,22 @@
 
 int main(int argc, char* argv[]) {
 //    runTests();
+    if (argc == 1) {
+        printf("Run program with dictionary file path as parameter, please.\n");
+        return 0;
+    }
+
     PrefixTreeNode* prefixTree = NULL;
     char* fileName = argv[1];
     char word[MAX_STRING_LENGHT];
 
     prefixTree = readTreeFromFile(prefixTree, fileName);
     if (prefixTree == NULL) {
-        printf("can't open file\n");
+        printf("Can't open dictionary file.\n");
         return 0;
     }
 
+    printf(">");
     scanf("%s", &word);
     while (strcmp(word, EXIT_WORD) != 0) {
         if (isWordInTree(prefixTree, word)) {
@@ -26,6 +32,7 @@ int main(int argc, char* argv[]) {
         else {
             printf("No\n");
         }
+        printf(">");
         scanf("%s", &word);
     }
 
